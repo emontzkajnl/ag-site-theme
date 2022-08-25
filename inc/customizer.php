cc5b14d16f4f5f5f2f015dec70f9b3330ca184df
Eric Montzka <emontzka@jnlcom.com>
2022-08-25 16:37:05 -0500

    add secondary color to customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -10,10 +10,34 @@
  *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
+
+// function sanitize_hex_color( $color ) {
+// 	if ( '' === $color ) {
+// 		return '';
+// 	}
+ 
+// 	// 3 or 6 hex digits, or the empty string.
+// 	if ( preg_match( '|^#([A-Fa-f0-9]{3}){1,2}$|', $color ) ) {
+// 		return $color;
+// 	}
+// }
+
 function ag_sites_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->add_setting( 'secondary_color_display',  array(
+		'type'			=> 'option',
+		'capability'	=> 'edit_theme_options',
+		'default'		=> '#333333',
+		'transport' 	=> 'refresh',
+		'sanitize_callback' => 'sanitize_hex_color',
+	));
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'secondary_color', array(
+		'label' => __( 'Secondary Color', 'ag-sites'), 
+		'section' => 'colors',
+		'settings'		=> 'secondary_color_display'
+	)));
 
 	if ( isset( $wp_customize->selective_refresh ) ) {
 		$wp_customize->selective_refresh->add_partial(
@@ -59,3 +83,17 @@ function ag_sites_customize_preview_js() {
 	wp_enqueue_script( 'ag-sites-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _ag_site_theme_VERSION, true );
 }
 add_action( 'customize_preview_init', 'ag_sites_customize_preview_js' );
+
+function ag_sites_customize_css() { 
+	$secondary_color = get_option('secondary_color_display'); ?>
+<style type="text/css">
+	a, a:visited {
+		color: <?php echo $secondary_color; ?>;
+	}
+	#newsletter input[type="submit"], .category-wrap a {
+		background-color: <?php echo $secondary_color; ?>;
+	}
+</style>
+<?php }
+
+add_action( 'wp_head', 'ag_sites_customize_css' );
--- js/customizer.js
+++ js/customizer.js
@@ -39,4 +39,5 @@
 			}
 		} );
 	} );
+	console.log('customizer');
 }( jQuery ) );
