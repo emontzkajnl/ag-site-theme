34dcfaedb42bd5ccfdb18ba8edd9e663fee7d803
Eric Montzka <emontzka@jnlcom.com>
2022-12-12 08:49:38 -0600

    mega hero fixes

--- template-parts/content-megahero.php
+++ template-parts/content-megahero.php
@@ -1,7 +1,7 @@
 <?php 
     $umov = get_field('use_megahero_or_video' );
 	if ($umov && $umov !== 'none'):
-        echo '<div class="megahero" style="height: 600px; width: 100%;">';
+        echo '<div class="megahero">';
         if ($umov === 'image'): 
         $img = get_field('megahero_image'); ?>
         <div class="megahero_img" style="background-image: url(<?php echo $img['url']; ?>); ">
