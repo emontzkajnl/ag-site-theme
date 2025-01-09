8d986d56f7f7f0d7132eae3f7c87efc74f2bd2fe
emontzka <emontzka@jnlcom.com>
2025-01-09 14:36:09 -0600

    resove conflict

--- 404.php
+++ 404.php
@@ -18,12 +18,21 @@ get_header();
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ag-sites' ); ?></p>
-
-					<?php
-					the_content();
+			<div class="wp-block-columns ag-site-sidebar-layout">
+				<div class="wp-block-column on-page-search">
+				<p><?php esc_html_e( 'Sorry, we can\'t find what you\'re looking for. Try searching below:', 'ag-sites' ); ?></p>
+				<?php
+					// the_content();
 					get_search_form();
 					?>
+				</div>
+				<div class="wp-block-column">
+					<?php get_sidebar(); ?>
+				</div>
+
+				
+
+				
 
 				
 					
